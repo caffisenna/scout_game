466fdb29c04924d6513a46289db0b8beef14d42c
caffi <caffi.senna@gmail.com>
2021-03-30 23:39:33 +0900

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -38,13 +38,13 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">スカウトゲーム</a>
+                <a class="navbar-brand" href="{{ url('/games') }}">スカウトゲーム</a>
             </div>
             <div id="navbar" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
+                    <!-- <li class="active"><a href="{{ url('/games')}}">Home</a></li>
                     <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#contact">Contact</a></li> -->
                 </ul>
             </div>
             <!--/.nav-collapse -->
