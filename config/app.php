760b6155db85ea080c0f76fa3009bb81aa4e90ab
caffi <caffi.senna@gmail.com>
2021-03-30 19:58:34 +0900

    setup

--- config/app.php
+++ config/app.php
@@ -156,6 +156,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // scaffold
+        Laralib\L5scaffold\GeneratorsServiceProvider::class ,
+
     ],
 
     /*
